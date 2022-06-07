d06b5caf483a6f2c35068fc669a48eaeb070c9c9
Abdul Malik <mam7264@gmail.com>
2022-06-07 14:29:41 +0530

    Time: 96 ms (32.98%), Space: 14.9 MB (42.13%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sanitizeString(self,string):
+        newString = []
+        for char in string:
+            isNumber = 48<=ord(char)<=57
+            if char.isalpha() or isNumber:
+                if ord(char) <= 90 and not isNumber:
+                    char = chr(ord(char) + 32)
+                newString.append(char)
+        return "".join(newString)
+    
+    def isPalindromeHelper(self,string):
+        first = 0
+        last = len(string)-1
+        
+        while first < last:
+            if string[first] != string[last]:
+                return False
+            first += 1
+            last -= 1
+            
+        return True
+    
+    def isPalindrome(self, s):
+        s = self.sanitizeString(s)
+        print(s)
+        return self.isPalindromeHelper(s)
+        

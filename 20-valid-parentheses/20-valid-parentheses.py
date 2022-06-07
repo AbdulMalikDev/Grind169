212e9eaf1b926bb035c59917b6d435e66a515590
Abdul Malik <mam7264@gmail.com>
2022-06-07 13:11:47 +0530

    Time: 22 ms (68.08%), Space: 13.6 MB (33.86%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -27,15 +27,27 @@ class Solution(object):
                 stack.append(char)
                 
             else:
+                # Edge case if there are only closing brackets
                 if len(stack)==0:
                     return False
+                
                 # not same type of bracket
                 if not self.isSameType(stack[-1],char):
                     return False
+                
+                # If same type of bracket (Ideal case)
                 stack.pop()
                 
                 
         return len(stack) == 0
+    
+    
+    # Note how i've divided the code to be 'Modular' and divided it into functions 
+    # instead of throwing everything into one function.
+    # Also note function names and variables naming.
+    # Commenting to understand logic flow
+    # Also note how i've avoided using 'else' everywhere i'm using an 'if'
+    # you can only do that if you really understand the program logic flow
                 
         
         

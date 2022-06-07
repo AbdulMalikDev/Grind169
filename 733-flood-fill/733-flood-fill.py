edf026c852ff9b222bbf3738a070333ac935b472
Abdul Malik <mam7264@gmail.com>
2022-06-07 21:45:35 +0530

    Time: 104 ms (18.79%), Space: 13.4 MB (92.65%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        
+        directions = ((1,0),(-1,0),(0,1),(0,-1))
+        queue = deque([(sr,sc)])
+        visited = set()
+        rows = len(image)
+        cols = len(image[0])
+        startingColor = image[sr][sc]
+        while queue:
+            x,y = queue.popleft()
+            if (x,y) not in visited:
+                visited.add((x,y))
+                image[x][y] = newColor
+                for dx,dy in directions:
+                    xx = x + dx
+                    yy = y + dy
+                    if 0 <= xx < rows and 0 <= yy < cols and image[xx][yy]==startingColor:
+                        queue.append((xx,yy))
+                        
+                        
+        return image
+        

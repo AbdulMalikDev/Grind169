df93d97b61b21d491d8c847fcd90e8df8f7d3569
Abdul Malik <mam7264@gmail.com>
2022-06-07 21:46:23 +0530

    Time: 84 ms (41.29%), Space: 13.7 MB (36.71%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def floodFill(self, image, sr, sc, newColor):
         
+        # Idea is to perform a BFS on the matrix/image
+        
+        # Initializing variables
         directions = ((1,0),(-1,0),(0,1),(0,-1))
         queue = deque([(sr,sc)])
         visited = set()
         rows = len(image)
         cols = len(image[0])
         startingColor = image[sr][sc]
+        
+        # Standard BFS Logic
         while queue:
             x,y = queue.popleft()
             if (x,y) not in visited:

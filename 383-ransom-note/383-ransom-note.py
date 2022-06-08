97e28d52b0e4c1b90f596241249fbbee22c96600
Abdul Malik <mam7264@gmail.com>
2022-06-08 09:00:50 +0530

    Time: 56 ms (77.20%), Space: 13.8 MB (37.34%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,22 +1,43 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         
-        # O(NlogN) Time | O(1) Space
         
-        n = len(ransomNote)
-        first = 0
-        second = 0
-        ransomNote = sorted(ransomNote) #O(NlogN) sorting
-        magazine = sorted(magazine)
+        # Bruteforce O(N*M) where N is ransomNote len and M magazine len
+        # Check every letter in ransomNote with Magazine
+
+        
+        # O(NlogN) Time | O(1) Space
+#         n = len(ransomNote)
+#         first = 0
+#         second = 0
+#         ransomNote = sorted(ransomNote) #O(NlogN) sorting
+#         magazine = sorted(magazine)
+        
+#         for char in magazine:
+#             if char == ransomNote[first]:
+#                 first += 1
+#                 if first == n:
+#                     return True
+                
+#         return False
+
+        # O(N) Time | O(N) Space
+    
+        letters = defaultdict(int)
         
         for char in magazine:
-            if char == ransomNote[first]:
-                first += 1
-                if first == n:
-                    return True
-                
-                
-        return False
+            letters[char] += 1
+            
+        for char in ransomNote:
+            if char not in letters or letters[char] <= 0:
+                return False
+            letters[char] -= 1
+            
+        return True
+            
+        
+
+        
                 
         
         

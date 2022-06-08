810124548da80bf1096fb02a5885c15f3268df13
Abdul Malik <mam7264@gmail.com>
2022-06-08 08:53:17 +0530

    Time: 133 ms (25.55%), Space: 13.7 MB (60.14%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         
+        # O(NlogN) Time | O(1) Space
+        
         n = len(ransomNote)
         first = 0
         second = 0
-        ransomNote = sorted(ransomNote)
+        ransomNote = sorted(ransomNote) #O(NlogN) sorting
         magazine = sorted(magazine)
         
-        # O(N) Time | O(1) Space
         for char in magazine:
             if char == ransomNote[first]:
                 first += 1

d2dbcaa985e7ffc56b70ad593b4bf12076c1fdad
Abdul Malik <mam7264@gmail.com>
2022-06-08 14:40:06 +0530

    Time: 22 ms (69.91%), Space: 13.5 MB (31.09%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def removeBackspace(self,string):
+        
+        backspace = 0
+        newString = ""
+        pointer = 0
+        while pointer < len(string):
+            
+            if string[pointer] == "#":
+                backspace += 1
+                
+            else:
+                if backspace > 0:
+                    backspace -= 1
+                else:
+                    newString = newString + string[pointer]
+                
+            pointer += 1
+        return newString[::-1]
+    
+    def backspaceCompare(self, s, t):
+        
+        # trick / intuition : REVERSE the strings
+        
+        s = self.removeBackspace(s[::-1])
+        t = self.removeBackspace(t[::-1])
+        return s == t
+        
+        
+                
+                
+        # print(newS[::-1])
+        
+        
+                
+            
+        

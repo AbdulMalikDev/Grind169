1e1f818f2ed3cafa70bf4b5f7c1b89127a2ce374
Abdul Malik <mam7264@gmail.com>
2022-06-09 12:18:55 +0530

    Time: 58 ms (12.96%), Space: 13.9 MB (56.17%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,23 +1,41 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        memo = defaultdict(int)
-        def stairs(n):
-            
-            if n in memo:
-                return memo[n]
-            
-            if n == 0:
-                return 1
-            elif n < 0:
-                return 0
-            
-            memo[n-1] = stairs(n-1)
-            memo[n-2] = stairs(n-2)
-            
-            return memo[n-1] + memo[n-2]
         
-        return stairs(n)
+        # memoization
+        # recording values of overlapping recursive calls
+        # decreases the complexity
+        
+        # top down
+#         memo = defaultdict(int)
+#         def stairs(n):
+            
+#             if n in memo:
+#                 return memo[n]
+            
+#             if n == 0:
+#                 return 1
+#             elif n < 0:
+#                 return 0
+            
+#             memo[n-1] = stairs(n-1)
+#             memo[n-2] = stairs(n-2)
+            
+#             return memo[n-1] + memo[n-2]
+        
+#         return stairs(n)
+
+
+        # bottom up
+        dp = [0] * (n+1)
+        
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[-1]
         
         
         

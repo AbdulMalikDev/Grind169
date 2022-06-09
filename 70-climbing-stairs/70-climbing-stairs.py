54a4432601e8d176f447fc0b30c5ee5ac223342b
Abdul Malik <mam7264@gmail.com>
2022-06-09 12:23:56 +0530

    Time: 39 ms (59.30%), Space: 13.9 MB (11.22%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -29,10 +29,13 @@ class Solution:
         # bottom up
         dp = [0] * (n+1)
         
-        dp[0] = 1
+        dp[0] = 0
         dp[1] = 1
+        if n==1:
+            return 1
+        dp[2] = 2
         
-        for i in range(2,n+1):
+        for i in range(3,n+1):
             dp[i] = dp[i-1] + dp[i-2]
             
         return dp[-1]

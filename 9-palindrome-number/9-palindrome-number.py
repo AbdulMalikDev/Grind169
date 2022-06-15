62876e9c51ba3c9c330f71ef93c11a6b81f2bd8a
Abdul Malik <mam7264@gmail.com>
2022-06-15 09:57:28 +0530

    Time: 91 ms (49.44%), Space: 13.9 MB (15.63%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,4 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        
+        if x < 0 or (x%10 == 0 and x!=0):
+            return False
+        
+        reversedNumber = 0
+        while reversedNumber < x:
+            # print(reversedNumber)
+            # print(x)
+            # print("")
+            # add last digit of x to reversed number
+            reversedNumber = reversedNumber * 10 + (x % 10)
+            # Getting rid of last digit of x
+            x = x // 10
+        
+        return (x == reversedNumber or (x == (reversedNumber // 10))) 
         

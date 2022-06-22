4fb2ee73ab49f1aafc24d236d1d5c9fe8f898034
Abdul Malik <mam7264@gmail.com>
2022-06-22 12:32:40 +0530

    Time: 956 ms (71.04%), Space: 20.4 MB (59.17%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -4,10 +4,17 @@ class Solution:
         result = []
         heap = []
         for x,y in points:
-            heapq.heappush(heap,(x**2+y**2,(x,y)))
+            dist = x**2+y**2
+            if len(heap) < k:
+                heapq.heappush(heap,(-dist,(x,y)))
+            else:
+                if dist < -heap[0][0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap,(-dist,(x,y)))
+                
             
-        for _ in range(k):
-            distance,coords = heapq.heappop(heap)
-            result.append(list(coords))
+#         for _ in range(k):
+#             distance,coords = heapq.heappop(heap)
+#             result.append(list(coords))
             
-        return result
+        return [list(y) for x,y in heap ]

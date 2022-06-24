5bccacee337d2c3978e34fa656650ad20dcebefb
Abdul Malik <mam7264@gmail.com>
2022-06-24 19:09:53 +0530

    Time: 2955 ms (15.60%), Space: 43.4 MB (7.35%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,24 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        memo = defaultdict(int)
+        def change(changeSum):
+            
+            if changeSum in memo:
+                return memo[changeSum]
+            
+            if changeSum == 0:
+                return 0
+            elif changeSum < 0:
+                return sys.maxsize
+            
+            result = sys.maxsize
+            for coin in coins:
+                if coin <= changeSum:
+                    result = min(result,1+change(changeSum-coin))
+            memo[changeSum] = result
+            return result
+        
+        result = change(amount)
+        return -1 if result==sys.maxsize else result
+        

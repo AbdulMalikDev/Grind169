0197ca2e25cd38f6db4dda019e4a0b0a782fb3f0
Abdul Malik <mam7264@gmail.com>
2022-06-24 19:37:07 +0530

    Time: 356 ms (40.94%), Space: 21.1 MB (55.78%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,6 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
+        # Numbers:     2     3    4     5
+        # Lefts:       1     2   2*3  2*3*4
+        # Rights:    3*4*5  4*5   5     1
+        
         result = [1]
         
         # store products from left in result array
@@ -9,16 +13,13 @@ class Solution:
             if i > 0:
                 result.append(result[-1] * nums[i-1])
                 
-        print(result)
         right = 1
         # for right just maintain a varaible from the right side
         for i in reversed(range(len(nums))):
-            # if i == 0:
-            #     result[i] = right
-            #     continue
             result[i] = result[i] * right
             right = right * nums[i]
-        print(result)
+            
+            
         return result
                 
                 

86ac94578a969dd2b3630972f091bd728c51a925
Abdul Malik <mam7264@gmail.com>
2022-06-28 12:20:08 +0530

    Time: 649 ms (5.00%), Space: 15.5 MB (5.13%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -9,16 +9,14 @@ class Solution:
         
         for char in s:
             freq[char] += 1
-        freqToChar = defaultdict(int)
+
         for char,frequency in freq.items():
             nums[frequency] += 1
-        print(freq)
-        # pri
+            
         while nums and nums[-1] == 0:
             nums.pop()
-        print(nums)
+            
         pointer = len(nums) - 1
-        
         while pointer > 0:
             num = nums[pointer]
             if num == 0 and running > 0:
@@ -32,14 +30,9 @@ class Solution:
             
             pointer -= 1
             
-        return result if result > 0 else 0
+        return result
             
-#         for num in nums:
-            
-#             if num == 0:
-#                 result -= 1
-#             elif num > 1:
-#                 result += (num-1)
+
                 
         
             

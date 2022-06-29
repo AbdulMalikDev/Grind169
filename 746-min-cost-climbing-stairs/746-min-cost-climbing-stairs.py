81f6d7ea10dc84c1a9b8bc6665247f2b15530b6b
Abdul Malik <mam7264@gmail.com>
2022-06-29 12:52:09 +0530

    Time: 121 ms (13.14%), Space: 14.1 MB (44.82%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,20 +1,37 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        memo = defaultdict(int)
         
-        def steps(index):
+        # Tabulation
+        
+        dp = [0 for _ in range(len(cost)+1)]
+        
+        dp[1] = cost[0]
+        dp[2] = cost[1]
+        
+        for i in range(3,len(cost)+1):
+            dp[i] = min(dp[i-1],dp[i-2]) + cost[i-1]
+        
+        
+        return min(dp[-1],dp[-2])
+        
+        
+#         memoized Version
+        
+#         memo = defaultdict(int)
+        
+#         def steps(index):
             
-            if index >= len(cost):
-                return 0
-            if index in memo:
-                return memo[index]
+#             if index >= len(cost):
+#                 return 0
+#             if index in memo:
+#                 return memo[index]
             
-            memo[index] = min(steps(index+1),steps(index+2)) + cost[index] 
-            return memo[index]
+#             memo[index] = min(steps(index+1),steps(index+2)) + cost[index] 
+#             return memo[index]
         
         
-        return min(steps(0),steps(1))
+#         return min(steps(0),steps(1))
         
         
         

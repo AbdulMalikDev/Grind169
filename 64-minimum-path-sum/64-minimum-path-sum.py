e8e586fdc34fab423e3b68d7add56043b4556eda
Abdul Malik <mam7264@gmail.com>
2022-06-29 13:19:26 +0530

    Time: 151 ms (51.52%), Space: 15.8 MB (30.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,21 +1,49 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
+        
+        # Tabulation
         rows = len(grid)
         cols = len(grid[0])
-        memo = defaultdict(int)
         
-        def minPath(r,c):
+        dp = [[0 for _ in range(cols+1)] for _ in range(rows+1)]
+        
+        # base case
+        for i in range(2,cols+1):
+            dp[0][i] = sys.maxsize
+        for j in range(2,rows+1):
+            dp[j][0] = sys.maxsize
             
-            if not 0<=r<rows or not 0<=c<cols:
-                return sys.maxsize
-            if r == rows-1 and c == cols-1:
-                return grid[r][c]
-            if (r,c) in memo:
-                return memo[(r,c)]
             
-            memo[(r,c)] = min(minPath(r+1,c),minPath(r,c+1)) + grid[r][c] 
-            return memo[(r,c)]
+        for r in range(1,rows+1):
+            for c in range(1,cols+1):
+                
+                # 'grid[r-1][c-1]' because dp is 1-indexed
+                dp[r][c] = min(dp[r-1][c],dp[r][c-1]) + grid[r-1][c-1]
+                
+        # for row in dp:
+        #     print(row)
+            
+        return dp[rows][cols]
+        
+        
+        
+#         Memoization
+#         rows = len(grid)
+#         cols = len(grid[0])
+#         memo = defaultdict(int)
+        
+#         def minPath(r,c):
+            
+#             if not 0<=r<rows or not 0<=c<cols:
+#                 return sys.maxsize
+#             if r == rows-1 and c == cols-1:
+#                 return grid[r][c]
+#             if (r,c) in memo:
+#                 return memo[(r,c)]
+            
+#             memo[(r,c)] = min(minPath(r+1,c),minPath(r,c+1)) + grid[r][c] 
+#             return memo[(r,c)]
         
         
         

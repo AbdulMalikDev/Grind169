d18e61b6d526c3b19099f319847bdc929789e21f
Abdul Malik <mam7264@gmail.com>
2022-08-23 09:30:12 -0400

    Time: 106 ms (32.47%), Space: 14.1 MB (57.41%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,29 +1,32 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        stack = [len(nums2)-1]
+        # N -> no.of elements in nums2
+        # M -> no.of elements in nums1
         
-        nextGreater = [0] * (len(nums2)-1) + [-1]
+        # O(N) Space
+        stack = [nums2[-1]]
         
+        nextGreater = defaultdict(int)
+        nextGreater[nums2[-1]] = -1
+        
+        # O(N) Time
         for i in reversed(range(0,len(nums2)-1)):
             currElement = nums2[i]
             # we want to find next "greater element"
-            while stack and nums2[stack[-1]] < currElement:
+            while stack and stack[-1] < currElement:
                 stack.pop()
                 
-            nextGreater[i] = stack[-1] if stack else -1
+            nextGreater[currElement] = stack[-1] if stack else -1
                 
             # insert this element
-            stack.append(i)
+            stack.append(currElement)
             
             
         result = []
+        # O(M) Time
         for num in nums1:
-            index = nums2.index(num)
-            if nextGreater[index] == -1:
-                result.append(-1)
-                continue
-            result.append(nums2[nextGreater[index]])
+            result.append(nextGreater[num])
             
         return result
         

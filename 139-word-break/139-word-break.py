4ec37f6f098d7af720f9b6eaff7f59d649c415d1
Abdul Malik <mam7264@gmail.com>
2022-08-27 15:31:35 -0400

    Time: 441 ms (5.07%), Space: 20.3 MB (5.10%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        words = set(wordDict)
+        memo = defaultdict()
+        
+        def areWordsContained(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if s[i:j] in words:
+                memo[(i,j)] = True
+                return True
+            
+            result = False
+            for k in range(i+1,j):
+                # print(i)
+                # print(k)
+                # print(j)
+                result = result or (areWordsContained(i,k) and areWordsContained(k,j))
+                
+            memo[(i,j)] = result
+            return result
+        
+        
+        return areWordsContained(0,len(s))
+        

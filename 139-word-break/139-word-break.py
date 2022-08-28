750fe8bf17d86a74bc1b5c75aad16a21b69f561a
Abdul Malik <mam7264@gmail.com>
2022-08-28 08:34:41 -0400

    Time: 611 ms (5.13%), Space: 20.1 MB (5.04%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -7,15 +7,13 @@ class Solution:
         def areWordsContained(i,j):
             if (i,j) in memo:
                 return memo[(i,j)]
+            
             if s[i:j] in words:
                 memo[(i,j)] = True
                 return True
             
             result = False
             for k in range(i+1,j):
-                # print(i)
-                # print(k)
-                # print(j)
                 result = result or (areWordsContained(i,k) and areWordsContained(k,j))
                 
             memo[(i,j)] = result

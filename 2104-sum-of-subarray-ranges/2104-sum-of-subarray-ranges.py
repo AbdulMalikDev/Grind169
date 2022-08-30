fb503b4866b69060fec94178bd7e03241073e800
Abdul Malik <mam7264@gmail.com>
2022-08-30 17:59:46 -0400

    Time: 197 ms (57.70%), Space: 14.2 MB (11.36%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,42 +1,43 @@
 class Solution:
-    
     def getMin(self,nums):
         stack = []
         result = [0] * len(nums)
-        previousLess = [-1] * len(nums)
+        previousLess = [-1] *  len(nums)
+        
         for i in range(len(nums)):
             while stack and nums[stack[-1]] > nums[i]:
                 stack.pop()
                 
             if stack:
                 previousLess[i] = stack[-1]
-                
-            stack.append(i)
             
+            stack.append(i)
+        
         for i in range(len(nums)):
-            result[i] = result[previousLess[i]] + (i-previousLess[i]) * nums[i]
+            result[i] = result[previousLess[i]] + (i - previousLess[i])*nums[i]
             
         return sum(result)
     
     def getMax(self,nums):
         stack = []
         result = [0] * len(nums)
-        previousLess = [-1] * len(nums)
+        previousLess = [-1] *  len(nums)
+        
         for i in range(len(nums)):
             while stack and nums[stack[-1]] < nums[i]:
                 stack.pop()
                 
             if stack:
                 previousLess[i] = stack[-1]
-                
-            stack.append(i)
             
+            stack.append(i)
+        
         for i in range(len(nums)):
-            result[i] = result[previousLess[i]] + (i-previousLess[i]) * nums[i]
+            result[i] = result[previousLess[i]] + (i - previousLess[i])*nums[i]
             
         return sum(result)
-    
+            
     def subArrayRanges(self, nums: List[int]) -> int:
-        
-        # print(self.getMin(nums))
         return self.getMax(nums) - self.getMin(nums)
+        
+        

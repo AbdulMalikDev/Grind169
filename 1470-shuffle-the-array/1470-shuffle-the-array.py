c701fc18fbbc308082d005a83e8428860b63f818
Abdul Malik <mam7264@gmail.com>
2022-10-29 17:54:40 -0400

    Time: 121 ms (56.73%), Space: 14.1 MB (41.21%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,21 +1,10 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        a = nums[:n]
-        b = nums[n:]
         result = []
-        i = 0
-        j = 0
-        k = 0
-        flag = True
-        while i < len(nums):
-            if flag:
-                result.append(a[j])
-                j+=1
-            else:
-                result.append(b[k])
-                k+=1
-            i+=1
-            flag = not flag
+        
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i+n])
             
         return result
             
